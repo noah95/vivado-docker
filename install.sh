4ef7037ca001cfa247172f7e4695df5db2793fa4
Noah Huetter <noahhuetter@gmail.com>
2018-01-17 22:52:58 +0100

    Fixed IP

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-IP=`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
+#IP=`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
+IP=192.168.0.27
 docker build \
     --build-arg VIVADO_TAR_HOST=$IP:8000 \
     --build-arg VIVADO_TAR_FILE=Xilinx_Vivado_SDK_2017.2_0616_1 \
